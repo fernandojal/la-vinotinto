4e2144263f00ad9e567b1e97252d4528e621fc4e
Ian Mande <isaac@mftech.io>
2022-08-20 17:47:55 -0400

    added and config msw

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,12 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 // Store
-
 import reportWebVitals from './reportWebVitals';
+
+if (process.env.SERVICE_MOCK) {
+  const { worker } = require('../mocks/browser');
+  worker.start();
+}
 
 const rootElement = document.getElementById('root');
 const root = rootElement && createRoot(rootElement);
